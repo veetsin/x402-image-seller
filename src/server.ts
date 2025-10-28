db21c2e8c7ae119ff03382c1ba8daf2923d7378e
Veetsin <ntqinwei@foxmail.com>
2025-10-29 00:38:42 +0800

    fix: test

--- src/server.ts
+++ src/server.ts
@@ -150,9 +150,6 @@ function paymentMiddleware(priceInUSDC: string) {
                             "error": "Payment Invalid",
                             "providedTxHash": "0x0d2ed13c84e496f424cc222e240474463ce6d18c0e3179a16b9e8f4e84758d36"
                         }
-                    },
-                    "data": {
-                        "data": []
                     }
                 });
             }
